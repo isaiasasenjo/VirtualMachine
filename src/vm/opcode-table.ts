a6ddce36b9e256d932c2d6e1d4cc98ae64e8eeb2
Eduard Isaias <isaiasasenjo@gmail.com>
2023-04-03 20:47:29 +0200

    Cambio de algunas opcodes

--- src/vm/opcode-table.ts
+++ src/vm/opcode-table.ts
@@ -6,8 +6,7 @@ const _op = (code: number, name: string, nargs: number = 0) => ({
 
 export const operations = [
   _op(0x00, "HALT"),
-  _op(0x50, "POP"),
-  _op(0x60, "PUSH", 1),
+
 
   _op(0x01, "ADD"),
   _op(0x02, "MUL"),
@@ -15,20 +14,23 @@ export const operations = [
   _op(0x04, "DIV"),
   _op(0x05, "MOD"),
 
-  _op(0x10, "LT"),
+  _op(0x10, "LT"), //16 en decimal
   _op(0x11, "GT"),
-  _op(0x14, "EQ"),
+  _op(0x14, "EQ"),  //20 en decimal
   _op(0x16, "AND"),
   _op(0x17, "OR"),
   _op(0x18, "XOR"),
-  _op(0x19, "NOT"),
+  _op(0x19, "NOT"), 
+  
+  _op(0x50, "POP"), //80 en decimal
+  _op(0x60, "PUSH", 1), //96 en decimal
 
-  _op(0x56, "JUMP"),
-  _op(0x57, "JUMPI"),
+  _op(0x56, "JUMP"), //86 en decimal
+  _op(0x57, "JUMPI"), //87 en decimal
 
-  _op(0x80, "DUP1"),
+  _op(0x80, "DUP1"), // 128 en decimal
   _op(0x81, "DUP2"),
-  _op(0x82, "DUP3"),
+  _op(0x82, "DUP3"), //130 en decimal
   _op(0x83, "DUP4"),
   _op(0x84, "DUP5"),
 
